d285e38a58609a4ada6977a4c4c55bf18c10a980
Eduardo Molina <edrdmolina11@gmail.com>
2021-11-16 09:30:25 -0500

    Allow CORS for Jokes and weather

--- controller/jokes.js
+++ controller/jokes.js
@@ -4,10 +4,12 @@ module.exports = {
     // Return one random joke.
     getJoke(req, res, next) {
         const idx = Math.floor(Math.random() * jokes.length);
+        res.set('Access-Control-Allow-Origin', '*');
         return res.status(200).json(jokes[idx]);
     },
     // Return entire Jokes Array.
     getJokes(req, res, next) {
+        res.set('Access-Control-Allow-Origin', '*');
         return res.status(200).json(jokes);
     }
 }
