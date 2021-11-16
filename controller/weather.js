bd467a6c86ba9863ac6622fd5585806c05759325
Eduardo Molina <edrdmolina11@gmail.com>
2021-11-16 18:47:14 -0500

    Add reverse GeoLocation

--- controller/weather.js
+++ controller/weather.js
@@ -1,25 +1,29 @@
 // Libraries
 const axios = require('axios');
+const nodeGeocoder = require('node-geocoder');
+const geoCoder = nodeGeocoder({ provider: 'openstreetmap' });
 
+// Open Weather API
 const oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall'
-const apiKey = process.env.OPENWEATHER_API_KEY;
+const openWeatherAPIKey = process.env.OPENWEATHER_API_KEY;
 
-const fs = require('fs')
+// const fs = require('fs')
 
 const savedData = require('../data/weatherData.json')
 
 module.exports = {
     async weatherController(req, res, next) {
-        // console.log(req.body)
         const { lat, lon, units, lang } = req.body;
+        //* Get weather data from open weather API
         // const weatherData = await axios.get(oneCallUrl, {
         //     params: {
-        //         lat, lon, appid: apiKey, units, lang
+        //         lat, lon, appid: openWeatherAPIKey, units, lang
         //     }
         // })
+        // if(!weatherData) return res.status(404);
+        // // console.log(weatherData.data);
 
-        // console.log(weatherData.data);
-
+        //* respond as JSON file to preserve API calls to openWeatherAPI. This is only done once to have response at hand.
         // fs.writeFile('weatherData.json', JSON.stringify(weatherData.data, null, 2), err => {
         //     if (err) {
         //         return console.log(err)
@@ -27,9 +31,14 @@ module.exports = {
         //     console.log('saved all data in json file')
         // })
 
+        // Reverse geocode to obtain location name.
+        const location = await geoCoder.reverse({ lat, lon})
+        if(!location) return res.status(404);
         res.status(200).json({
             // weatherData: weatherData.data,
-            weatherData: savedData
+            weatherData: savedData,
+            location
         })
     },
+
 }
