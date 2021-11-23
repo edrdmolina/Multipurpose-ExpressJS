0249f9e01600942e10871cb85170f604d0753758
Eduardo Molina <edrdmolina11@gmail.com>
2021-11-23 14:25:11 -0500

    Reafactored GeoCoding and Changed Providers

--- controller/weather.js
+++ controller/weather.js
@@ -1,62 +1,75 @@
 // Libraries
 const axios = require('axios');
 const nodeGeocoder = require('node-geocoder');
-const geoCoder = nodeGeocoder({ provider: 'openstreetmap' });
 
 // Open Weather API
 const oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall'
 const openWeatherAPIKey = process.env.OPENWEATHER_API_KEY;
+// Initialize Node Geo Coder
+const geoCoder = nodeGeocoder({ 
+    provider: 'mapquest', apiKey: process.env.MAPQUEST_API_KEY 
+});
 
 // const fs = require('fs')
 
 const savedData = require('../data/weatherData.json')
 
 module.exports = {
-    async weatherController(req, res, next) {
+    async weatherController(req, res) {
         const { lat, lon, units, lang } = req.body;
-        //* Get weather data from open weather API
-        // const weatherData = await axios.get(oneCallUrl, {
-        //     params: {
-        //         lat, lon, appid: openWeatherAPIKey, units, lang
-        //     }
-        // })
-        // if(!weatherData) return res.status(404);
-        // // console.log(weatherData.data);
 
+        /*
+        //* Get weather data from open weather API
+        const weatherData = await axios.get(oneCallUrl, {
+            params: {
+                lat, lon, appid: openWeatherAPIKey, units, lang
+            }
+        })
+        if(!weatherData) return res.status(404);
+        */
+
+        /*
         //* respond as JSON file to preserve API calls to openWeatherAPI. This is only done once to have response at hand.
-        // fs.writeFile('weatherData.json', JSON.stringify(weatherData.data, null, 2), err => {
-        //     if (err) {
-        //         return console.log(err)
-        //     }
-        //     console.log('saved all data in json file')
-        // })
+        fs.writeFile('weatherData.json', JSON.stringify(weatherData.data, null, 2), err => {
+            if (err) {
+                return console.log(err)
+            }
+            console.log('saved all data in json file')
+        })
+        */
 
         // Reverse geocode to obtain location name.
         const location = await geoCoder.reverse({ lat, lon})
-        if(!location) return res.status(404);
-        res.status(200).json({
+        if(!location.length) return res.status(404);
+        return res.status(200).json({
             // weatherData: weatherData.data,
             weatherData: savedData,
-            location
+            location: location[0]
         })
     },
-    async getLatLon(req, res, next) {
-        const { input } = req.body;
+    async geoLocation(req, res) {
+        const { countryCode, address } = req.body;
 
-        console.log(input)
+        const result = await geoCoder.geocode(address)
+        // console.log('result: ', result)
+        if(!result.length) {
+            return res.status(200).json({ 
+                error: 'No location found. Try a Different City or Zip Code at geocode'
+            })
+        }
 
-        const location = await geoCoder.geocode(input)
 
-        console.log(location)
+        // Filter out all locations except all that have country from input
+        const location = result.filter(r => r.countryCode === countryCode)
+        // console.log('location: ', location)
         if(!location.length) {
             return res.status(200).json({ 
-                error: 'No location found. Try a Different City or Zip Code'
+                error: 'No location found. Try a Different Zip Code'
             })
         }
 
         return res.status(200).json({
-            lat: location[0].latitude,
-            lon: location[0].longitude
+            location: location[0]
         })
     }
 
--- routes/weather.js
+++ routes/weather.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { weatherController, getLatLon } = require('../controller/weather');
+const { weatherController, geoLocation } = require('../controller/weather');
 const { catchAsync } = require('../middleware/index');
 
 router.post('/', catchAsync(weatherController));
 
-router.post('/location', catchAsync(getLatLon));
+router.post('/geolocation', catchAsync(geoLocation));
 
 module.exports = router;
