d9091faad4652350ce24690fb5637b9856096a6f
Eduardo Molina <edrdmolina11@gmail.com>
2022-04-14 19:07:37 -0400

    Portfolio visits

---
+++ controller/portfolio.js
@@ -0,0 +1,23 @@
+const Visit = require('../models/portfolio-models/visits');
+const { cleanIp } = require('../middleware/helpers');
+
+module.exports = {
+    async addVisit(req, res) {
+        const ipAddress = cleanIp(req.ip);
+	    const software = req.headers['user-agent'];
+        const prevVisit = await Visit.findOne({ ipAddress });
+        if(prevVisit) {
+            prevVisit.visitCount++;
+            prevVisit.lastVisit = Date();
+            prevVisit.save()
+            return res.status(200).json(prevVisit);
+        } else {
+            const newVisit = await new Visit({
+                ipAddress, software, 
+                visitCount: 1, lastVisit: Date()
+            })
+            newVisit.save()
+            return res.status(200).json(newVisit);
+        }
+    }
+}
